63d5ba00859d8ffdee3833876da0f4fa44699ee6
MatrikMoon <5924350+MatrikMoon@users.noreply.github.com>
2022-03-19 13:56:55 -0500

    Change to protoc-gen-ts

--- Scripts/build_proto_for_ts.ps1
+++ Scripts/build_proto_for_ts.ps1
@@ -2,4 +2,4 @@ if (Test-Path 'TS') {
     Remove-Item 'TS' -Recurse
 }
 New-Item -Path 'TS' -ItemType Directory
-protoc -I="../" "../*.proto" --js_out="TS" --ts_out="TS" --plugin=protoc-gen-ts=.\\node_modules\\.bin\\protoc-gen-ts.cmd
+protoc -I="../" "../*.proto" --ts_out="TS" --plugin=protoc-gen-ts=.\\node_modules\\.bin\\protoc-gen-ts.cmd --ts_opt=no_namespace
