10ab34f8b6cf9331931e79fec32a607c8009fed6
Moon <5924350+MatrikMoon@users.noreply.github.com>
2022-08-17 21:31:53 -0500

    Use protobuf-ts to generate ts
    Yes, this is the one I landed on after being confused for months. The right one.

--- Scripts/build_proto_for_ts.ps1
+++ Scripts/build_proto_for_ts.ps1
@@ -1,5 +1,8 @@
 if (Test-Path 'TS') {
     Remove-Item 'TS' -Recurse
 }
+if (-Not (Test-Path 'node_modules')) {
+    npm i
+}
 New-Item -Path 'TS' -ItemType Directory
-protoc -I="../" "../*.proto" --js_out="import_style=commonjs,binary:TS" --ts_out="TS" --plugin=protoc-gen-ts=.\\node_modules\\.bin\\protoc-gen-ts.cmd
+npx protoc -I="../" "../*.proto" --ts_out="TS"
--- Scripts/build_proto_for_ts.sh
+++ Scripts/build_proto_for_ts.sh
@@ -7,4 +7,4 @@ if [[ -d ./TS ]]; then
     rm -rf ./TS
 fi
 mkdir ./TS
-protoc -I=../ ../*.proto --ts_out="TS" --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts --ts_opt=no_namespace
+npx protoc -I="../" "../*.proto" --ts_out="TS"
