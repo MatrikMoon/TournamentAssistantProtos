70ac880c42bd828dedcaebcbd6e1bf194b43ce19
Dannypoke03 <daniel.carruthers@outlook.com>
2022-10-03 22:18:41 +1100

    revert ts packet scripts

--- Scripts/build_proto_for_ts.sh
+++ Scripts/build_proto_for_ts.sh
@@ -7,16 +7,4 @@ if [[ -d ./TS ]]; then
     rm -rf ./TS
 fi
 mkdir ./TS
-protoc -I=../ ../*.proto --ts_out="TS"
-
-for FILE in ./TS/*.ts; do 
-    echo "Processing $FILE"; 
-    sed -i -r 's/\#one_of_decls:/private one_of_decls:/g' $FILE;
-    sed -i -r 's/\#one_of_decls/one_of_decls/g' $FILE;
-    sed -i -r 's/proto.models/Models/g' $FILE;
-    sed -i -r 's/proto.packet/Packets/g' $FILE;
-    sed -i -r 's/message.difficulties = reader.readPackedInt32\(\);/var values = \(reader.isDelimited\(\) ? reader.readPackedInt32\(\) : [reader.readInt32\(\)]\);\
-                        for \(var i = 0; i < values.length; i++\) {\
-                            message.difficulties.push\(values[i]\);\
-                        }/g' $FILE;
-done
+npx protoc -I="../" "../*.proto" --ts_out="TS"
